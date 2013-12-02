db6b26adc2b94fcfd9345aa517a39b61d488ea20
daleroberts <dale.o.roberts@gmail.com>
2013-12-02 12:08:43 +1100

    Ensure compile under windows uses mingw

--- compile.cmd
+++ compile.cmd
@@ -2,6 +2,10 @@
 echo.
 echo Compiling C code for TCRM
 echo -------------------------
-python installer\setup.py build_ext -i
+copy installer\setup.py .
+copy installer\setup.cfg .
+python setup.py build_ext -i
+del setup.py
+del setup.cfg
 echo.
 pause
