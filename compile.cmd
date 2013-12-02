9fbad3de3964a0041d76528a4144dff254518865
Dale Roberts <dale.o.roberts@gmail.com>
2013-12-02 12:12:03 +1100

    Supress copy output

--- compile.cmd
+++ compile.cmd
@@ -2,8 +2,8 @@
 echo.
 echo Compiling C code for TCRM
 echo -------------------------
-@copy installer\setup.py .
-@copy installer\setup.cfg .
+@copy installer\setup.py . >nul
+@copy installer\setup.cfg . >nul
 python setup.py build_ext -i
 @del setup.py
 @del setup.cfg
