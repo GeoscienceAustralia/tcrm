5b02a70810cf53114efab21bef470e46a2fffe9e
daleroberts <dale.o.roberts@gmail.com>
2013-12-02 12:09:49 +1100

    Hide command outputs

--- compile.cmd
+++ compile.cmd
@@ -2,10 +2,10 @@
 echo.
 echo Compiling C code for TCRM
 echo -------------------------
-copy installer\setup.py .
-copy installer\setup.cfg .
+@copy installer\setup.py .
+@copy installer\setup.cfg .
 python setup.py build_ext -i
-del setup.py
-del setup.cfg
+@del setup.py
+@del setup.cfg
 echo.
 pause
