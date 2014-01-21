6a285b994f40b05ff64d47b793eda1ac94c39d2a
wcarthur <craig.arthur@ga.gov.au>
2014-01-21 15:54:25 +1100

    Pre-filter grid for spline interpolation

--- Utilities/interp3d.py
+++ Utilities/interp3d.py
@@ -10,13 +10,13 @@ Version: $Rev$
 Id: $Id$
 """
 
-import numpy
-import pdb
+import numpy as np
 from scipy.ndimage.interpolation import map_coordinates
 
 def interp3d(input_array, coords, 
              scale=[360., 180., 365.],
-             offset=[0.,-90.,0.]):
+             offset=[0.,-90.,0.],
+             prefilter=True):
     """
     Wrapper to scipy.ndimage.interpolation.map_coordinates, which
     converts coordinates of points to indices that correspond to the
@@ -51,8 +51,46 @@ def interp3d(input_array, coords,
 
     dims = input_array.shape
     indices = [d*(c - o) / s for d,c,o,s in 
-               zip(dims,coords,offset,scale)]
+               zip(dims, coords, offset, scale)]
 
-    values = map_coordinates(input_array,indices, mode='wrap')
+    values = map_coordinates(input_array, indices, mode='wrap',
+                             prefilter=prefilter)
 
     return values
+
+def _interp(data, coords, scale=[360., 180.], offset=[0., -90.]):
+    """
+    Wrapper to scipy.ndimage.interpolation.map_coordinates, which converts 
+    coordinates of points to indices that correspond to the array. 
+    We assume that one is working with lon, latdata (i.e. initially 
+    designed to work with daily long term mean sea level pressure)
+
+    Input:
+    input_array - a 2-d array of data at regular intervals, representing the 
+                  data to be evaluated
+    coords - a 2xn array of coordinates at which the data in input_array
+             will be interpolated
+    scale - a scale factor that reduces the coords values to the range of
+            indices in input_array
+    offset - an offset factor that is subtracted from the coords values
+             before adjusting the scale (above)
+
+    Output:
+    1-d array of values corresponding to the interpolated values 
+    at the points given in 'coords'
+
+    Example: vals = interp2d( data, coords, scale=[360., 180.],
+                              offset=[0.,-90.] )
+    """
+
+    if data.ndim != np.asarray(coords).ndim:
+        raise ValueError('Input array and coordinates do not have matching dimensions')
+    
+    dims = np.array(data.shape)
+    
+    indices = [d*(c - o) / s for d, c, o, s in 
+                zip(dims, coords, offset, scale)]
+
+    values = map_coordinates(data, indices, mode='wrap')
+    
+    return values    
