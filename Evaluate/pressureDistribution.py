5e78342714c2fdbbcb21147ef2310f7a3488933b
wcarthur <craig.arthur@ga.gov.au>
2014-05-22 15:52:06 +1000

    Enable save() method

--- Evaluate/pressureDistribution.py
+++ Evaluate/pressureDistribution.py
@@ -5,7 +5,7 @@
 .. module:: PressureDistribution
     :synopsis: calculate central pressure distributions based on historic and
                synthetic event sets
-               
+
 .. moduleauthor:: Craig Arthur <craig.arthur@ga.gov.au>
 
 """
@@ -90,9 +90,9 @@ def readMultipleTrackData(trackfile):
     :type  trackfile: str
     :param trackfile: the track data filename.
     """
-    
+
     log.debug("Reading multiple track data from {0}".format(trackfile))
-    
+
     datas = []
     data = readTrackData(trackfile)
     if len(data) > 0:
@@ -126,7 +126,7 @@ def loadTracks(trackfile):
         track.trackId = (i, n)
         tracks.append(track)
     return tracks
-    
+
 class gridCell(object):
     def __init__(self, xmin, ymin, xmax, ymax, number, index):
         self.xmin = xmin
@@ -147,17 +147,17 @@ def plotDensity(x, y, data, llLon=None, llLat=None, urLon=None, urLat=None,
     It updates the figure object, but does not have scope to save or display it.
     That way, the function can be used to generate a number of subplots, with
     the calling program handling the saving/display requirements.
-    
+
     :param x: :class:`numpy.ndarray` of longitude points
     :param y: :class:`numpy.ndarray` of latitude points
-    :param data: 2-d :class:`numpy.ndarray` of data values. 
-    
+    :param data: 2-d :class:`numpy.ndarray` of data values.
+
     """
 
     if (len(x.shape) < 2) and (len(y.shape) < 2):
         [xx, yy] = np.meshgrid(x, y)
     else:
-        xx = x        
+        xx = x
         yy = y
     if llLon:
         llcrnrlon = llLon
@@ -193,7 +193,7 @@ def plotDensity(x, y, data, llLon=None, llLat=None, urLon=None, urLat=None,
 
     # Set the colour map:
     cmap = pyplot.get_cmap(cmap)
-    
+
     if maskocean:
         try:
             from mpl_toolkits.basemap import maskoceans
@@ -230,7 +230,7 @@ def plotDensity(x, y, data, llLon=None, llLat=None, urLon=None, urLat=None,
     cb = pyplot.colorbar(aspect=30, orientation='vertical',
                          extend='both', pad=0.1)
     cb.ax.tick_params(direction='in')
-                         
+
     if cb.orientation == 'horizontal':
         for t in cb.ax.get_xticklabels():
             t.set_fontsize(8)
@@ -239,36 +239,36 @@ def plotDensity(x, y, data, llLon=None, llLat=None, urLon=None, urLat=None,
         cb.set_label(clabel)
 
     return
-    
+
 class PressureDistribution(object):
     def __init__(self, configFile):
         """
         Calculate central pressure distributions on a grid
-        
+
         :param str configFile: path to a TCRM configuration file.
         """
 
         config = ConfigParser()
         config.read(configFile)
         self.configFile = configFile
-    
+
         # Define the grid:
         gridLimit = config.geteval('Region', 'gridLimit')
         gridSpace = config.geteval('Region', 'GridSpace')
-        
-        self.lon_range = np.arange(gridLimit['xMin'], 
+
+        self.lon_range = np.arange(gridLimit['xMin'],
                                    gridLimit['xMax'] + 0.1,
                                    gridSpace['x'])
-        self.lat_range = np.arange(gridLimit['yMin'], 
+        self.lat_range = np.arange(gridLimit['yMin'],
                                    gridLimit['yMax'] + 0.1,
                                    gridSpace['y'])
 
         outputPath = config.get('Output', 'Path')
         self.trackPath = pjoin(outputPath, 'tracks')
         self.plotPath = pjoin(outputPath, 'plots', 'stats')
-        self.dataPath = pjoin(outputPath, 'process','stats')
+        self.dataPath = pjoin(outputPath, 'process')
 
-        self.synNumYears = config.getint('TrackGenerator', 
+        self.synNumYears = config.getint('TrackGenerator',
                                          'yearspersimulation')
         cellnumber = 0
         self.gridCells = []
@@ -281,16 +281,16 @@ class PressureDistribution(object):
                 self.gridCells.append(gridCell(xmin, ymin, xmax, ymax,
                                                cellnumber, (k, l)))
                 cellnumber += 1
-                                         
+
     def calculate(self, tracks):
 
-        dataMean = ma.zeros((len(self.lon_range) - 1, 
+        dataMean = ma.zeros((len(self.lon_range) - 1,
                              len(self.lat_range) - 1))
-        dataMin = ma.zeros((len(self.lon_range) - 1, 
+        dataMin = ma.zeros((len(self.lon_range) - 1,
                             len(self.lat_range) - 1))
-        dataMax = ma.zeros((len(self.lon_range) - 1, 
+        dataMax = ma.zeros((len(self.lon_range) - 1,
                             len(self.lat_range) - 1))
-        dataMed = ma.zeros((len(self.lon_range) - 1, 
+        dataMed = ma.zeros((len(self.lon_range) - 1,
                             len(self.lat_range) - 1))
 
         for cell in self.gridCells:
@@ -303,7 +303,7 @@ class PressureDistribution(object):
                 if len(ii) > 0:
                     vv = t.CentralPressure[ii].compress(t.CentralPressure[ii] < sys.maxint)
                     vcell = np.append(vcell, vv.compress(vv > 0.0))
-                    
+
             if len(vcell > 0):
                 dataMean[cell.index[0], cell.index[1]] = np.mean(vcell)
                 dataMin[cell.index[0], cell.index[1]] = np.min(vcell)
@@ -315,17 +315,17 @@ class PressureDistribution(object):
         dataMax = ma.masked_equal(dataMax, 0)
         dataMed = ma.masked_equal(dataMed, 0)
         return dataMean, dataMin, dataMax, dataMed
-        
+
     def calcMinPressure(self, tracks):
         minCP = np.zeros(len(tracks))
-        
+
         for i, t in enumerate(tracks):
             minCP[i] = t.CentralPressure.min()
-            
+
         bins = np.arange(850., 1020., 5.)
         h, n = np.histogram(minCP, bins, normed=True)
         return h
-        
+
     def historic(self):
         """Load historic data and calculate histogram"""
         log.info("Processing historical pressure distributions")
@@ -343,54 +343,54 @@ class PressureDistribution(object):
         else:
             self.histMean, self.histMin, \
                 self.histMax, self.histMed = self.calculate(tracks)
-                
+
             self.histMinCP = self.calcMinPressure(tracks)
 
     def synthetic(self):
         """Load synthetic data and calculate histogram"""
         log.info("Processing synthetic pressure distributions")
-        
+
         filelist = os.listdir(self.trackPath)
         trackfiles = [pjoin(self.trackPath, f) for f in filelist
                       if f.startswith('tracks')]
-        synMean = -9999. * ma.ones((len(trackfiles), 
-                                    len(self.lon_range) - 1, 
+        synMean = -9999. * ma.ones((len(trackfiles),
+                                    len(self.lon_range) - 1,
                                    len(self.lat_range) - 1))
-        synMin = -9999. * ma.ones((len(trackfiles), 
-                                   len(self.lon_range) - 1, 
+        synMin = -9999. * ma.ones((len(trackfiles),
+                                   len(self.lon_range) - 1,
                                    len(self.lat_range) - 1))
-        synMax = -9999. * ma.ones((len(trackfiles), 
-                                   len(self.lon_range) - 1, 
+        synMax = -9999. * ma.ones((len(trackfiles),
+                                   len(self.lon_range) - 1,
                                    len(self.lat_range) - 1))
-        synMed = -9999. * ma.ones((len(trackfiles), 
-                                   len(self.lon_range) - 1, 
+        synMed = -9999. * ma.ones((len(trackfiles),
+                                   len(self.lon_range) - 1,
                                    len(self.lat_range) - 1))
-                     
+
         bins = np.arange(850., 1020., 5.)
         synMinCP = np.empty((len(trackfiles),
                              len(bins) - 1))
-                            
+
         for n, trackfile in enumerate(trackfiles):
             tracks = loadTracks(trackfile)
             synMean[n, :, :],  synMin[n, :, :], \
                synMax[n, :, :], synMed[n, :, :] = self.calculate(tracks)
             synMinCP[n, :] = self.calcMinPressure(tracks)
-            
+
         synMean = ma.masked_values(synMean, -9999.)
         synMin = ma.masked_values(synMin, -9999.)
         synMed = ma.masked_values(synMed, -9999.)
         synMax = ma.masked_values(synMax, -9999.)
-        
+
         self.synMean = ma.mean(synMean, axis=0)
         self.synMed = ma.mean(synMed, axis=0)
         self.synMin = ma.mean(synMin, axis=0)
         self.synMax = ma.mean(synMax, axis=0)
-        
+
         self.synMeanUpper = percentile(ma.compressed(synMean), per=95, axis=0)
         self.synMeanLower = percentile(ma.compressed(synMean), per=5, axis=0)
         self.synMinUpper = percentile(ma.compressed(synMin), per=95, axis=0)
         self.synMinLower = percentile(ma.compressed(synMin), per=5, axis=0)
-                    
+
         self.synMinCPDist = np.mean(synMinCP, axis=0)
         self.synMinCPLower = percentile(synMinCP, per=5, axis=0)
         self.synMinCPUpper = percentile(synMinCP, per=95, axis=0)
@@ -398,22 +398,22 @@ class PressureDistribution(object):
     def plotPressureMean(self):
         """
         Plot a map of observed and synthetic mean pressure values
-        
+
         """
-        
+
         datarange = (950, 1000)
         pyplot.figure()
         ax1 = pyplot.subplot(211)
-        plotDensity(self.lon_range[:-1], self.lat_range[:-1], 
+        plotDensity(self.lon_range[:-1], self.lat_range[:-1],
                     np.transpose(self.histMean), datarange=datarange,
                     clabel="Mean central pressure (hPa)", cmap='gist_heat')
         ax1.text(self.lon_range[1], self.lat_range[1],"Historic",
                  bbox=dict(fc='white', ec='black', alpha=0.5))
-                    
+
         ax2 = pyplot.subplot(212)
-        plotDensity(self.lon_range[:-1], self.lat_range[:-1], 
+        plotDensity(self.lon_range[:-1], self.lat_range[:-1],
                     np.transpose(self.synMean), datarange=datarange,
-                    clabel="Mean central pressure (hPa)", cmap='gist_heat') 
+                    clabel="Mean central pressure (hPa)", cmap='gist_heat')
         ax2.text(self.lon_range[1], self.lat_range[1],"Mean synthetic",
                  bbox=dict(fc='white', ec='black', alpha=0.5))
         pyplot.savefig(pjoin(self.plotPath, 'meanPressure.png'))
@@ -421,78 +421,78 @@ class PressureDistribution(object):
     def plotPressureMin(self):
         """
         Plot a map of observed and synthetic minimum central pressure values.
-        
+
         """
-        
+
         datarange = (900, 1000)
         pyplot.figure()
         ax1 = pyplot.subplot(211)
-        plotDensity(self.lon_range[:-1], self.lat_range[:-1], 
+        plotDensity(self.lon_range[:-1], self.lat_range[:-1],
                     np.transpose(self.histMin), datarange=datarange,
                     clabel="Minimum central pressure (hPa)", cmap='gist_heat')
         ax1.text(self.lon_range[1], self.lat_range[1],"Historic",
                  bbox=dict(fc='white', ec='black', alpha=0.5))
-                    
+
         ax2 = pyplot.subplot(212)
-        plotDensity(self.lon_range[:-1], self.lat_range[:-1], 
+        plotDensity(self.lon_range[:-1], self.lat_range[:-1],
                     np.transpose(self.synMin), datarange=datarange,
-                    clabel="Minimum central pressure (hPa)", cmap='gist_heat') 
+                    clabel="Minimum central pressure (hPa)", cmap='gist_heat')
         ax2.text(self.lon_range[1], self.lat_range[1],"Mean synthetic",
                  bbox=dict(fc='white', ec='black', alpha=0.5))
         pyplot.savefig(pjoin(self.plotPath, 'minPressure.png'))
-    
+
     def plotPressureMinDiff(self):
         """
-        Plot a map of the difference between observed and synthetic minimum 
+        Plot a map of the difference between observed and synthetic minimum
         pressure values.
-        
+
         """
-        
+
         datarange = (-50, 50)
         pyplot.figure()
         ax1 = pyplot.subplot(111)
         data = self.histMin - self.synMin
-        plotDensity(self.lon_range[:-1], self.lat_range[:-1], 
+        plotDensity(self.lon_range[:-1], self.lat_range[:-1],
                     np.transpose(data), datarange=datarange,
                     clabel="Minimum central pressure difference (hPa)", cmap='rwbcmap')
         ax1.text(self.lon_range[1], self.lat_range[1],"Historic - mean synthetic",
                  bbox=dict(fc='white', ec='black', alpha=0.5))
-                    
+
         pyplot.savefig(pjoin(self.plotPath, 'minPressureDiff.png'))
-    
+
     def plotPressureMeanDiff(self):
         """
-        Plot a map of the difference between observed and synthetic mean 
+        Plot a map of the difference between observed and synthetic mean
         pressure values.
-        
+
         """
-        
+
         datarange = (-50, 50)
         pyplot.figure()
         ax1 = pyplot.subplot(111)
         data = self.histMean - self.synMean
-        plotDensity(self.lon_range[:-1], self.lat_range[:-1], 
+        plotDensity(self.lon_range[:-1], self.lat_range[:-1],
                     np.transpose(data), datarange=datarange,
                     clabel="Mean central pressure difference (hPa)", cmap='rwbcmap')
         ax1.text(self.lon_range[1], self.lat_range[1],"Historic - mean synthetic",
                  bbox=dict(fc='white', ec='black', alpha=0.5))
-                    
+
         pyplot.savefig(pjoin(self.plotPath, 'meanPressureDiff.png'))
-        
+
     def plotMinPressureDistribution(self):
         """
         Plot a pdf of observed minimum central pressure values, and the mean
         of the synthetic event sets (plus 90th percentile values).
-        
+
         """
-        
+
         pyplot.figure()
         ax1 = pyplot.subplot(111)
         bins = np.arange(850., 1020., 5.)
         ax1.plot(bins[:-1], self.histMinCP, color='r', lw=2)
         ax1.plot(bins[:-1], self.synMinCPDist, color='k', lw=2)
         ax1.fill_between(bins[:-1], self.synMinCPUpper,
-                         self.synMinCPLower, facecolor='0.75', 
+                         self.synMinCPLower, facecolor='0.75',
                          edgecolor='0.99', alpha=0.7)
 
         pyplot.xlim(850., 1020.)
@@ -504,8 +504,86 @@ class PressureDistribution(object):
 
         outputFile = pjoin(self.plotPath, 'minPressureDist.png')
         pyplot.savefig(outputFile)
-        
-        
+
+    def save(self):
+        dataFile = pjoin(self.dataPath, 'pressureDistribution.nc')
+
+        # Simple sanity check (should also include the synthetic data):
+        if not hasattr(self, 'hist'):
+            log.critical("No historical data available!")
+            log.critical("Check that data has been processed before trying to save data")
+            return
+
+        log.info('Saving track density data to {0}'.format(dataFile))
+        dimensions = {
+            0: {
+                'name': 'lat',
+                'values': self.lat_range[:-1],
+                'dtype': 'f',
+                'atts': {
+                    'long_name': 'Latitude',
+                    'units': 'degrees_north',
+                    'axis': 'Y'
+                }
+            },
+            1: {
+                'name': 'lon',
+                'values': self.lon_range[:-1],
+                'dtype': 'f',
+                'atts': {
+                    'long_name': 'Longitude',
+                    'units':'degrees_east',
+                    'axis': 'X'
+                }
+            }
+        }
+
+        # Define variables:
+        variables = {
+            0: {
+                'name': 'hist_mean',
+                'dims': ('lat', 'lon'),
+                'values': np.transpose(self.histMean),
+                'dtype': 'f',
+                'atts': {
+                    'long_name': 'Historical mean central pressure',
+                    'units':'hPa'
+                }
+            },
+            1: {
+                'name': 'syn_mean',
+                'dims': ('lat', 'lon'),
+                'values': np.transpose(self.synMean),
+                'dtype': 'f',
+                'atts': {
+                    'long_name': 'Mean synthetic mean central pressure',
+                    'units':'hPa'
+                }
+            },
+            2: {
+                'name': 'hist_min',
+                'dims': ('lat', 'lon'),
+                'values': np.transpose(self.histMin),
+                'dtype': 'f',
+                'atts': {
+                    'long_name': 'Historical minimum central pressure',
+                    'units':'hPa'
+                }
+            },
+            3: {
+                'name': 'syn_min',
+                'dims': ('lat', 'lon'),
+                'values': np.transpose(self.synMin),
+                'dtype': 'f',
+                'atts': {
+                    'long_name': 'Mean synthetic minimum central pressure',
+                    'units': 'hPa'
+                }
+            }
+        }
+
+        ncSaveGrid(dataFile, dimensions, variables)
+
     def run(self):
         """Run the pressure distribution evaluation"""
         self.historic()
@@ -513,9 +591,10 @@ class PressureDistribution(object):
 
         self.plotPressureMean()
         self.plotPressureMin()
-        
+
         self.plotPressureMeanDiff()
         self.plotPressureMinDiff()
 
-        self.plotMinPressureDistribution()        
-        
+        self.plotMinPressureDistribution()
+
+        self.save()
--- Evaluate/trackDensity.py
+++ Evaluate/trackDensity.py
@@ -4,7 +4,7 @@
 
 .. module:: TrackDensity
    :synopsis: Calculate density of TC tracks over a grid (TCs/degree/year)
-   
+
 .. moduleauthor: Craig Arthur <craig.arthur@ga.gov.au>
 
 """
@@ -130,7 +130,7 @@ def loadTracksFromPath(path):
     msg = 'Processing %d track files in %s' % (len(trackfiles), path)
     log.info(msg)
     return loadTracksFromFiles(sorted(trackfiles))
-    
+
 def plotDensity(x, y, data, llLon=None, llLat=None, urLon=None, urLat=None,
                 res='i', dl=5., datarange=(-1.,1.), cmap='gist_heat_r', title=None,
                 xlab='Longitude', ylab='Latitude', clabel=None, maskland=False,
@@ -142,11 +142,11 @@ def plotDensity(x, y, data, llLon=None, llLat=None, urLon=None, urLat=None,
     It updates the figure object, but does not have scope to save or display it.
     That way, the function can be used to generate a number of subplots, with
     the calling program handling the saving/display requirements.
-    
+
     :param x: :class:`numpy.ndarray` of longitude points
     :param y: :class:`numpy.ndarray` of latitude points
-    :param data: 2-d :class:`numpy.ndarray` of data values. 
-    
+    :param data: 2-d :class:`numpy.ndarray` of data values.
+
     """
 
     if (len(x.shape) < 2) and (len(y.shape) < 2):
@@ -188,7 +188,7 @@ def plotDensity(x, y, data, llLon=None, llLat=None, urLon=None, urLat=None,
 
     # Set the colour map:
     cmap = pyplot.get_cmap(cmap)
-    
+
     if maskocean:
         try:
             from mpl_toolkits.basemap import maskoceans
@@ -225,7 +225,7 @@ def plotDensity(x, y, data, llLon=None, llLat=None, urLon=None, urLat=None,
     cb = pyplot.colorbar(aspect=30, orientation='vertical',
                          extend='max', pad=0.1)
     cb.ax.tick_params(direction='in')
-                         
+
     if cb.orientation == 'horizontal':
         for t in cb.ax.get_xticklabels():
             t.set_fontsize(8)
@@ -239,31 +239,31 @@ class TrackDensity(object):
     def __init__(self, configFile):
         """
         Calculate density of TC positions on a grid
-        
+
         :param str configFile: path to a TCRM configuration file.
         """
 
         config = ConfigParser()
         config.read(configFile)
         self.configFile = configFile
-    
+
         # Define the grid:
         gridLimit = config.geteval('Region', 'gridLimit')
         gridSpace = config.geteval('Region', 'GridSpace')
-        
-        self.lon_range = np.arange(gridLimit['xMin'], 
+
+        self.lon_range = np.arange(gridLimit['xMin'],
                                    gridLimit['xMax'] + 0.1,
                                    gridSpace['x'])
-        self.lat_range = np.arange(gridLimit['yMin'], 
+        self.lat_range = np.arange(gridLimit['yMin'],
                                    gridLimit['yMax'] + 0.1,
                                    gridSpace['y'])
 
         outputPath = config.get('Output', 'Path')
         self.trackPath = pjoin(outputPath, 'tracks')
         self.plotPath = pjoin(outputPath, 'plots', 'stats')
-        self.dataPath = pjoin(outputPath, 'process','stats')
+        self.dataPath = pjoin(outputPath, 'process')
 
-        self.synNumYears = config.getint('TrackGenerator', 
+        self.synNumYears = config.getint('TrackGenerator',
                                          'yearspersimulation')
 
 
@@ -275,13 +275,13 @@ class TrackDensity(object):
 
         :param tracks: Collection of :class:`Track` objects.
         """
-        
+
         lon = []
         lat = []
 
         for t in tracks:
             lon = np.append(lon, t.Longitude)
-            lat = np.append(lat, t.Latitude)  
+            lat = np.append(lat, t.Latitude)
         histogram, x, y = np.histogram2d(lon, lat,
                                          [self.lon_range,
                                           self.lat_range],
@@ -294,9 +294,9 @@ class TrackDensity(object):
         config.read(self.configFile)
         inputFile = config.get('DataProcess', 'InputFile')
         source = config.get('DataProcess', 'Source')
-        
+
         timestep = config.getfloat('TrackGenerator', 'Timestep')
-        
+
         path, base = os.path.split(inputFile)
         interpHistFile = pjoin(path, "interp_tracks.csv")
         try:
@@ -319,33 +319,33 @@ class TrackDensity(object):
                 endYr = max(endYr, max(t.Year))
             numYears = endYr - startYr
             self.hist = self.calculate(tracks) / numYears
-             
+
 
 
     def synthetic(self):
         """Load synthetic data and calculate histogram"""
-        
+
         #config = ConfigParser()
         #config.read(self.configFile)
         #timestep = config.getfloat('TrackGenerator', 'Timestep')
-        
+
         filelist = os.listdir(self.trackPath)
         trackfiles = [pjoin(self.trackPath, f) for f in filelist
                       if f.startswith('tracks')]
-        self.synHist = -9999. * np.ones((len(trackfiles), 
-                                         len(self.lon_range) - 1, 
+        self.synHist = -9999. * np.ones((len(trackfiles),
+                                         len(self.lon_range) - 1,
                                      len(self.lat_range) - 1))
         for n, trackfile in enumerate(trackfiles):
             tracks = loadTracks(trackfile)
             self.synHist[n, :, :] = self.calculate(tracks) / self.synNumYears
-            
+
         self.synHist = ma.masked_values(self.synHist, -9999.)
         self.synHistMean = ma.mean(self.synHist, axis=0)
         self.medSynHist = ma.median(self.synHist, axis=0)
 
         self.synHistUpper = percentile(ma.compressed(self.synHist), per=95, axis=0)
         self.synHistLower = percentile(ma.compressed(self.synHist), per=5, axis=0)
-                    
+
     def save(self):
         dataFile = pjoin(self.dataPath, 'density.nc')
 
@@ -426,71 +426,71 @@ class TrackDensity(object):
                 }
             }
         }
-        
+
         ncSaveGrid(dataFile, dimensions, variables)
-                        
+
     def plotHistoric(self):
         """
         Plot results
         """
-        
+
         datarange = (0, self.hist.max())
-        plotDensity(self.lon_range[:-1], self.lat_range[:-1], 
+        plotDensity(self.lon_range[:-1], self.lat_range[:-1],
                     np.transpose(self.hist), datarange=datarange)
-                    
+
     def plotSynthetic(self):
         """
         Plot results from synthetic events
         """
         datarange = (0, self.synHistMean.max())
-        plotDensity(self.lon_range[:-1], self.lat_range[:-1], 
-                    np.transpose(self.synHistMean), datarange=datarange) 
-                    
+        plotDensity(self.lon_range[:-1], self.lat_range[:-1],
+                    np.transpose(self.synHistMean), datarange=datarange)
+
     def plotTrackDensity(self):
-        
+
         datarange = (0, self.hist.max())
         pyplot.figure()
         ax1 = pyplot.subplot(211)
-        plotDensity(self.lon_range[:-1], self.lat_range[:-1], 
+        plotDensity(self.lon_range[:-1], self.lat_range[:-1],
                     np.transpose(self.hist), datarange=datarange,
                     clabel="TC observations/yr")
         ax1.text(self.lon_range[1], self.lat_range[1],"Historic",
                  bbox=dict(fc='white', ec='black', alpha=0.5))
-                    
+
         ax2 = pyplot.subplot(212)
-        plotDensity(self.lon_range[:-1], self.lat_range[:-1], 
+        plotDensity(self.lon_range[:-1], self.lat_range[:-1],
                     np.transpose(self.synHistMean), datarange=datarange,
-                    clabel="TC observations/yr") 
+                    clabel="TC observations/yr")
         ax2.text(self.lon_range[1], self.lat_range[1],"Mean synthetic",
                  bbox=dict(fc='white', ec='black', alpha=0.5))
         pyplot.savefig(pjoin(self.plotPath, 'track_density.png'))
-                    
+
     def plotTrackDensityPercentiles(self):
-        
+
         datarange = (0, self.hist.max())
         pyplot.figure()
         ax1 = pyplot.subplot(211)
-        plotDensity(self.lon_range[:-1], self.lat_range[:-1], 
+        plotDensity(self.lon_range[:-1], self.lat_range[:-1],
                     np.transpose(self.synHistUpper), datarange=datarange,
                     clabel="TC observations/yr")
         ax1.text(self.lon_range[1], self.lat_range[1],"Upper percentile",
                  bbox=dict(fc='white', ec='black', alpha=0.5))
-                    
+
         ax2 = pyplot.subplot(212)
-        plotDensity(self.lon_range[:-1], self.lat_range[:-1], 
+        plotDensity(self.lon_range[:-1], self.lat_range[:-1],
                     np.transpose(self.synHistLower), datarange=datarange,
                     clabel="TC observations/yr")
         ax2.text(self.lon_range[1], self.lat_range[1],"Lower percentile",
                  bbox=dict(fc='white', ec='black', alpha=0.5))
         pyplot.savefig(pjoin(self.plotPath, 'track_density_percentiles.png'))
 
-                 
+
     def run(self):
-        
+
         self.historic()
         self.synthetic()
-        
+
         self.plotTrackDensity()
         self.plotTrackDensityPercentiles()
-        
-        #self.save()
+
+        self.save()
