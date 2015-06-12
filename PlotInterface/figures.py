3f540eb6c49a0796ae83392098af845bdb733a48
wcarthur <craig.arthur@ga.gov.au>
2015-06-12 12:58:18 +1000

    Use seaborn for handling figure asthetics
    `seaborn` automatically sets up "nice" looking figure parameters (e.g.
    linewidths, label sizes, fonts, etc.), so the fundamental classes for
    plotting used in TCRM are now using this module.

--- PlotInterface/figures.py
+++ PlotInterface/figures.py
@@ -1,3 +1,15 @@
+"""
+:mod:`figures` -- basic figure elements
+=======================================
+
+.. module:: figures
+    :synopsis: Basic figure elements for statistical and other plotting 
+               actions.
+
+.. moduleauthor:: Craig Arthur <craig.arthur@ga.gov.au>
+
+"""
+
 from __future__ import division
 
 import sys
@@ -5,14 +17,14 @@ import numpy as np
 import scipy.stats as stats
 import wind.windmodels as windmodels
 
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.artist import setp
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from mpl_toolkits.basemap import Basemap
 
-import seaborn
-seaborn.set(style="ticks")
+import seaborn as sns
+sns.set(style="ticks")
 
 class WindProfileFigure(Figure):
 
@@ -56,7 +68,6 @@ class WindProfileFigure(Figure):
                 pass
 
         ax.legend(legend)
-        ax.grid()
         ax.set_xlabel('Radius (km)', fontsize=14)
         ax.set_ylabel('Wind speed (m/s)', fontsize=14)
         ax.set_title((r'$P_c = %d\hspace{0.5}hPa,\hspace{1} P_e' +
@@ -69,70 +80,69 @@ class ScatterHistogramFigure(Figure):
     def __init__(self):
         Figure.__init__(self)
 
-    def render(self, axes, x, y):
-        color = axes._get_lines.color_cycle
-
-        axes.scatter(x, y, color=color.next(), s=3)
-        axes.set_aspect('equal')
-
-        divider = make_axes_locatable(axes)
-        axesTop = divider.append_axes('top', 1.2, pad=0.1, sharex=axes)
-        axesRight = divider.append_axes('right', 1.2, pad=0.2, sharey=axes)
-
-        # Make labels invisible:
-        setp(axesTop.get_xticklabels() + axesRight.get_yticklabels(),
-                    visible=False)
-
-        axesTop.hist(x, bins=20, fc='k', ec=None, normed=True)
-        axesRight.hist(y, bins=20, fc='k', ec=None, orientation='horizontal', normed=True)
-        [t.set_rotation(-90) for t in axesRight.get_xticklabels()]
-
-
     def plot(self, xdata, ydata):
         i = np.where((xdata < sys.maxint) & (ydata < sys.maxint))[0]
         x = xdata[i]
         y = ydata[i]
 
-        axes = self.add_subplot(1, 1, 1)
-        self.render(axes, x, y)
+        jp = sns.jointplot(x, y, kind='reg',
+                           joint_kws={'scatter_kws':{
+                                          'color':'slategray',
+                                          'alpha':0.5
+                                          }
+                                      },
+                           **kwargs)
+        self.tight_layout()
+
 
 class QuantileFigure(Figure):
-    
+    """
+    Create a quantile-quantile plot that includes estimated confidence
+    intervals for the simulated (or second) dataset. 
+
+    """
+
     def __init__(self):
         Figure.__init__(self)
         self.subfigures = []
 
     def percentiles(self, data):
         """Calculate percentile values from 1 to 100"""
-        p = np.array([stats.scoreatpercentile(data, q) for q in range(1, 101)])
-        return p
+        per = np.array([stats.scoreatpercentile(data, q) 
+                        for q in range(1, 101)])
+        return per
 
     def percentilerange(self, data):
         """Calculate a range of percentile values"""
-        b = np.zeros((1000,100))
+        samples = np.zeros((1000, 100))
         for n in xrange(1000):
-            b[n, :] = np.array([np.random.choice(data) for _ in xrange(100)])
+            samples[n, :] = np.array([np.random.choice(data) 
+                                      for _ in xrange(100)])
         
-        d = stats.scoreatpercentile(b, range(1,101), axis=1)
-        u = stats.scoreatpercentile(d, 95, axis=1)
-        l = stats.scoreatpercentile(d, 5, axis=1)
-        return u, l
+        dummy = stats.scoreatpercentile(samples, range(1, 101), axis=1)
+        upper = stats.scoreatpercentile(dummy, 95, axis=1)
+        lower = stats.scoreatpercentile(dummy, 5, axis=1)
+        return upper, lower
 
     def add(self, xdata, ydata, axisrange, xlabel='x', ylabel='y',
             title='Q-Q plot'):
-
+        """Add a subplot to the collection"""
         self.subfigures.append((xdata, ydata, axisrange, 
                                 xlabel, ylabel, title))
 
     def formatAxes(self, axes, limits):
+        """Format the axes to have the same limits"""
         axes.set_ylim(limits)
         axes.set_xlim(limits)
 
-        #axes.set_xticks(np.linspace(*limits, num=11))
-        #axes.set_yticks(np.linspace(*limits, num=11))
-
     def addGrid(self, axes):
-        axes.grid(True, which='major', color='k', linestyle='-', linewidth=0.1)
+        """
+        Add a grid to the subplot axes.
+        
+        :param axes: `matplotlib.axes` instance
+
+        """
+        axes.grid(True)
 
 
     def subplot(self, axes, subfigure):
@@ -144,11 +154,11 @@ class QuantileFigure(Figure):
         xupper, xlower = self.percentilerange(xdata)
         yupper, ylower = self.percentilerange(ydata)
         
-        axes.scatter(xper, yper, s=5, c='r', marker='o', 
+        axes.scatter(xper, yper, c='r', marker='o', 
                      edgecolor='none', alpha=0.5)
 
-        axes.plot(xupper, ylower, color='0.5', linestyle='solid', linewidth=0.5)
-        axes.plot(xlower, yupper, color='0.5', linestyle='solid', linewidth=0.5)
+        axes.plot(xupper, ylower, color='0.5', linewidth=1)
+        axes.plot(xlower, yupper, color='0.5', linewidth=1)
 
         axes.set_xlabel(xlabel)
         axes.set_ylabel(ylabel)
@@ -177,7 +187,7 @@ class RegressionFigure(Figure):
         self.subfigures = []
 
     def add(self, xdata, ydata, xlabel='x', ylabel='y',
-        title='x', transform=lambda x: x):
+            title='x', transform=lambda x: x):
         self.subfigures.append((xdata, ydata, xlabel, ylabel, title, transform))
 
     def prepareData(self, xdata, ydata, transform):
@@ -190,7 +200,7 @@ class RegressionFigure(Figure):
             d = np.abs(data - np.median(data))
             mdev = np.median(d)
             s  = d/mdev if mdev else 0
-            return data[s<m]
+            return data[s < m]
 
         xmin = filterOutliers(xdata).min()
         xmax = filterOutliers(xdata).max()
@@ -209,22 +219,12 @@ class RegressionFigure(Figure):
         color = axes._get_lines.color_cycle
 
         xdata, ydata = self.prepareData(xdata, ydata, transform)
-
         k = color.next()
-        axes.plot(xdata, ydata, '.', alpha=0.8, color=k)
+        scatter_kws = {'color':k,
+                       'alpha':0.5}
+        sns.regplot(xdata, ydata, ax=axes, scatter_kws=scatter_kws)
 
         xmin, xmax = self.formatAxes(axes, xdata, ydata)
-
-        m, c, r, p, e = stats.linregress(np.array([xdata, ydata]))
-        x = np.array([xmin, xmax])
-        y = m * x + c
-
-        k = color.next()
-        axes.plot(x, x, '-', color=k)
-
-        k = color.next()
-        axes.plot(x, y, '-', color=k, label='r = %5.3f' % r)
-
         axes.set_xlabel(xlabel)
         axes.set_ylabel(ylabel)
         axes.set_title(title)
@@ -233,13 +233,13 @@ class RegressionFigure(Figure):
         legend.get_frame().set_alpha(0.5)
 
     def plot(self):
-        n = len(self.subfigures)
-        r = int(np.ceil(np.sqrt(n)))
-        c = int(np.ceil(n / r))
-        w, h = self.get_size_inches()
-        self.set_size_inches(w * c, r * h)
+        nfig = len(self.subfigures)
+        rows = int(np.ceil(np.sqrt(nfig)))
+        cols = int(np.ceil(nfig / rows))
+        width, height = self.get_size_inches()
+        self.set_size_inches(width * cols, rows * height)
         for i, subfigure in enumerate(self.subfigures):
-            axes = self.add_subplot(r, c, i + 1)
+            axes = self.add_subplot(rows, cols, i + 1)
             self.subplot(axes, subfigure)
         
 
@@ -273,20 +273,11 @@ class LineRegressionFigure(RegressionFigure):
         xdata, ydata = self.prepareData(xdata, ydata, transform)
 
         k = color.next()
-        axes.plot(xdata, ydata, '-', alpha=0.8, color=k)
+        scatter_kws = {'color':k,
+                       'alpha':0.5}
+        sns.regplot(xdata, ydata, ax=axes, scatter_kws=scatter_kws)
 
         xmin, xmax = self.formatAxes(axes, xdata, ydata)
-
-        m, c, r, p, e = stats.linregress(np.array([xdata, ydata]))
-        x = np.array([xmin, xmax])
-        y = m * x + c
-
-        k = color.next()
-        axes.plot(x, x, '-', color=k)
-
-        k = color.next()
-        axes.plot(x, y, '-', color=k, label='r = %5.3f' % r)
-
         axes.set_xlabel(xlabel)
         axes.set_ylabel(ylabel)
         axes.set_title(title)
@@ -324,7 +315,8 @@ class BearingFigure(LaggedRegressionFigure):
 class FrequencyFigure(LineRegressionFigure):
 
     def plot(self, years, frequency):
-        self.add(np.array(years[1:-1], int), frequency[1:-1], 'Year', 'Frequency', 'Annual Frequency')
+        self.add(np.array(years[1:-1], int), frequency[1:-1], 'Year', 
+                 'Frequency', 'Annual Frequency')
         super(FrequencyFigure, self).plot()
 
 
@@ -341,7 +333,8 @@ def saveWindProfilesFigure(lat, lon, eP, cP, rMax, beta,
     saveFigure(fig, filename)
 
 
-def savePressureFigure(pressures, pressureRates, filename='docs/prs_corr.png'):
+def savePressureFigure(pressures, pressureRates, 
+                       filename='docs/prs_corr.png'):
     fig = PressureFigure()
     fig.plot(pressures, pressureRates)
     saveFigure(fig, filename)
