996c611a9fad132e927670037ed317b0e221b62e
Brett Walker <brett.walker@ga.gov.au>
2017-07-17 19:58:35 +1000

    possible fix to issue, requires more testing

--- Utilities/columns.py
+++ Utilities/columns.py
@@ -36,10 +36,10 @@ def colReadCSV(configFile, dataFile, source):
     cols = config.get(source, 'Columns').split(delimiter)
 
     usecols = [i for i, c in enumerate(cols) if c != 'skip']
+    names = [c for c in cols if c != 'skip']
     try:
         data = np.genfromtxt(dataFile, dtype=None, delimiter=delimiter,
-                             usecols=usecols, comments=None,
-                             skip_header=numHeadingLines,
+                             usecols=usecols, names=names, skip_header=numHeadingLines,
                              autostrip=True)
     except IOError:
         log.exception("File not found: {0}".format(dataFile))
@@ -49,7 +49,5 @@ def colReadCSV(configFile, dataFile, source):
                        "or generator.").format(dataFile))
         raise TypeError(("{0} is not a string, filehandle "
                          "or generator.").format(dataFile))
-
-    data.dtype.names = [c for c in cols if c != 'skip']
 
     return data
