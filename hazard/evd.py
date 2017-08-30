869ab75a8f6d93c0af2f9258a9bf478fca9f957c
Claire Krause <claire.krause@ga.gov.au>
2017-08-30 16:25:15 +1000

    Added line-by-line comments to EVD code

--- hazard/evd.py
+++ hazard/evd.py
@@ -40,7 +40,8 @@ def estimateEVD(v, years, missingValue=-9999., minRecords=50, yrspersim=1):
     Return period values are not calculated if the shape parameter is negative
     or zero.
 
-    :param v: array of data values.
+    :param v: array of data values. Values represent max events for each year 
+              of simulation at a single grid box
     :type v: :class:`numpy.ndarray`
     :param years: array of years for which to calculate return period values.
     :type years: :class:`numpy.ndarray`
@@ -50,10 +51,13 @@ def estimateEVD(v, years, missingValue=-9999., minRecords=50, yrspersim=1):
     :param int yrspersim: data represent block maxima - this gives the length
                           of each block in years.
 
-    :return: return period values
-    :rtype: :class:`numpy.ndarray`
-    :return: location, shape and scale parameters of the distribution
-    :rtype: float
+    Returns:
+    --------
+
+    :param w: `numpy.array` of return period wind speed values
+    :param loc: location parameter
+    :param scale: scale parameter
+    :param shp: shape parameter
 
     """
     # Convert to float to prevent integer division & ensure consistent data
@@ -64,24 +68,28 @@ def estimateEVD(v, years, missingValue=-9999., minRecords=50, yrspersim=1):
 
     # Initialise variables:
     loc, scale, shp = [missingValue, missingValue, missingValue]
-    w = missingValue * np.ones(len(years))
+    w = missingValue * np.ones(len(years)) # Create empty return period array
 
-    if (v.max() > 0.):
-        ii = np.flatnonzero(v)
+    if (v.max() > 0.): # Check for valid data
+        ii = np.flatnonzero(v) # Return indices for non-zero elements of the flattened aray
         # Only calculate l-moments for those grid points where the values are
         # not all equal, and where there are 50 or more valid (>0) values.
         if v[ii].min() != v[ii].max():
             if len(ii) >= minRecords:
-                l1, l2, l3 = lmom.samlmu(v, 3)
+                l1, l2, l3 = lmom.samlmu(v, 3) # find 3 l-moments
+                # t3 = L-skewness (Hosking 1990)
                 t3 = l3 / l2
                 if (l2 <= 0.) or (np.abs(t3) >= 1.):
                     # Reject points where the second l-moment is negative
-                    # or the ratio of the third to second is > 1.
+                    # or the ratio of the third to second is > 1, i.e. positive
+                    # skew.
                     log.debug("Invalid l-moments")
                 else:
                     # Parameter estimation returns the location, scale and
                     # shape parameters
                     xmom = [l1, l2, t3]
+                    # Calculates the parameters of the distribution given its
+                    # L-moments. GEV distribution calculated.
                     loc, scale, shp = np.array(lmom.pelgev(xmom))
                     # We only store the values if the first parameter is
                     # finite (i.e. the location parameter is finite)
@@ -90,10 +98,14 @@ def estimateEVD(v, years, missingValue=-9999., minRecords=50, yrspersim=1):
                                            missingValue,
                                            missingValue]
 
-    for i, t in enumerate(years):
+    # Calculate return period wind speeds
+    for i, t in enumerate(years): 
+        # if no valid fit was found, then there are no return period wind speeds
         if shp <= 0.:
             w[i] = missingValue
+        # if a valid fit was found...
         else:
+            # Calculate wind speed for each return period
             w[i] = (np.transpose(loc + (scale / shp) *
                     (1. - np.power(-1. * np.log(1. - (yrspersim / t)), shp))))
 
