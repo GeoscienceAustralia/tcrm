1d70feac067e783118997ce7319c9e04a0e8894e
Kieran Ricardo <kieranricardo@hotmail.com>
2022-01-13 14:42:04 +1100

    updated rmax to linear formula

--- TrackGenerator/trackSize.py
+++ TrackGenerator/trackSize.py
@@ -18,16 +18,14 @@ import statsmodels.api as sm
 
 LOG = logging.getLogger()
 
-def rmax(dp, lat, eps, coeffs=[3.5843946536979779,-0.0045486143609339436,
-                               0.78621467400844858, 0.0024030344245284741,
-                               0.0015567629057007433]):
+def rmax(dp, lat, eps, coeffs=[4.22, -0.0198, 0.0023]):
     """
     Calculate radius to maximum wind based on pressure deficit and
     latitude. This function allows for the random variate to be set
     when calling the function. Default coefficients for the functional
     form of ln(Rmw) are given, based on JTWC data for the southern hemisphere.
     
-    ln(Rmw) = a + b*dp + c*exp(-d*dp^2) + f*|lat| + eps
+    ln(Rmw) = a + b*dp + c*|lat| + eps
 
     eps is not included in the coefficients (though that may be considered
     by some to be more logical), so that it can remain constant for a single
@@ -43,18 +41,15 @@ def rmax(dp, lat, eps, coeffs=[3.5843946536979779,-0.0045486143609339436,
     :returns: radius to maximum wind value.
 
     """
-    if len(coeffs) < 4:
+    if len(coeffs) != 3:
         LOG.warn("Insufficient coefficients for rmw calculation!")
         LOG.warn("Using default values")
-        coeffs = [3.5843946536979779,-0.0045486143609339436,
-                  0.78621467400844858, 0.0024030344245284741,
-                  0.0015567629057007433]
+        coeffs = [4.22, -0.0198, 0.0023]
 
     if isinstance(dp, (np.ndarray, list)) and \
       isinstance(lat, (np.ndarray, list)):
         assert len(dp) == len(lat)
-    yy = coeffs[0] + coeffs[1]*dp + coeffs[2] * np.exp(-coeffs[3] * dp * dp) +\
-         coeffs[4] * np.abs(lat) + eps
+    yy = coeffs[0] + coeffs[1] * dp + coeffs[2] * np.abs(lat) + eps
     rm = np.exp(yy)
     return rm
 
